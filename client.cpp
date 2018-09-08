81047afea4357e235312795cc37608d4032ff655
Sébastien Wagener <sebastien.wagener@gmail.com>
2018-09-08 15:26:06 +0200

    Add endpoint parsing to client

--- client.cpp
+++ client.cpp
@@ -17,20 +17,57 @@ namespace po = boost::program_options;
 namespace ssl = boost::asio::ssl;
 namespace websocket = boost::beast::websocket;
 
-struct ClientArguments {
+struct Endpoint {
     std::string host;
     std::string port;
-    std::string path {"/"};
+    std::string path;
+
+    Endpoint(const std::string & s)
+        : port("443")
+        , path("/"){
+        using namespace std;
+        auto colon = s.find(":");
+        if (colon != string::npos) {
+            this->host = s.substr(0, colon);
+            auto slash = s.find("/");
+            this->port = s.substr(colon + 1, slash - colon - 1);
+            if (slash != string::npos) {
+                this->path = s.substr(slash);
+            }
+        } else {
+            // no port given
+            auto slash = s.find("/");
+            this->host = s.substr(0, slash);
+            if (slash != std::string::npos) {
+                this->path = s.substr(slash);
+            }
+        }
+    }
+};
+
+std::ostream & operator<<(std::ostream & os, Endpoint const & v) {
+    os << v.host << ":" << v.port << v.path;
+    return os;
+}
+
+struct ClientArguments {
+    Endpoint endpoint;
+    bool quiet;
+
+    ClientArguments(const Endpoint & endpoint, bool quiet)
+        : endpoint(endpoint)
+        , quiet(quiet)
+    {}
 };
 
 ClientArguments parseCommandLine(int argc, char** argv) {
     using namespace std;
     po::options_description desc("Allowed options");
-    ClientArguments clientArguments;
+    string endpoint;
     desc.add_options()
         ("help", "produce help message")
-        ("host,h", po::value(&clientArguments.host)->required(), "the IP or DNS name of the server to connect")
-        ("port,p", po::value(&clientArguments.port)->default_value("443"), "the port on the server to connect to")
+        ("endpoint", po::value(&endpoint)->required(), "the endpoint, e.g. localhost:443/path (default port: 443, default path: /)")
+        ("quiet", "Disable logging")
     ;
 
     po::variables_map vm;
@@ -41,7 +78,8 @@ ClientArguments parseCommandLine(int argc, char** argv) {
         std::cout << desc << "\n";
         exit(1);
     }
-    return clientArguments;
+    bool quiet = vm.count("quiet") > 0;
+    return {{endpoint}, quiet};
 }
 
 int main(int argc, char** argv)
@@ -51,6 +89,10 @@ int main(int argc, char** argv)
         const auto clientArguments = parseCommandLine(argc, argv);
         auto const text = "hello world";
 
+        if (!clientArguments.quiet) {
+            std::cout << "connecting to endpoint " << clientArguments.endpoint << std::endl;
+        }
+
         boost::asio::io_context ioc;
 
         ssl::context ctx{ssl::context::sslv23_client};
@@ -59,11 +101,11 @@ int main(int argc, char** argv)
         tcp::resolver resolver{ioc};
         websocket::stream<ssl::stream<tcp::socket>> ws{ioc, ctx};
 
-        auto const results = resolver.resolve(clientArguments.host, clientArguments.port);
+        auto const results = resolver.resolve(clientArguments.endpoint.host, clientArguments.endpoint.port);
         // TODO error handling
         boost::asio::connect(ws.next_layer().next_layer(), results.begin(), results.end());
         ws.next_layer().handshake(ssl::stream_base::client);
-        ws.handshake(clientArguments.host, clientArguments.path);
+        ws.handshake(clientArguments.endpoint.host, clientArguments.endpoint.path);
 
         // Send the message
         ws.write(boost::asio::buffer(std::string(text)));
