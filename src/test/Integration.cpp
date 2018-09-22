29db1f45d4e4afb373ff03db0f239abda3a7fffd
Sébastien Wagener <sebastien.wagener@gmail.com>
2018-09-22 23:54:09 +0200

    Add . as include_directory

--- src/test/Integration.cpp
+++ src/test/Integration.cpp
@@ -1,5 +1,13 @@
 #include <catch2/catch.hpp>
 
-TEST_CASE( "2: Factorial of 0 is 1 (fail)", "[multi-file:2]" ) {
-    REQUIRE( 0 == 0 );
+#include <networkstream/client/Client.h>
+
+using namespace networkstream;
+
+TEST_CASE( "client-server-roundtrip", "[integration]" ) {
+    auto const text = "hello world";
+    client::Arguments args { {"localhost:50000"}, false};
+    auto response = client::run(args, text);
+    std::cout << "got response: " << run(args, text) << std::endl;
+    REQUIRE( response == text);
 }
